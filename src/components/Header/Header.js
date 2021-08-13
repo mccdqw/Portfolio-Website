4adf733a79cf67a46e2d3d3ea7918a94c530ce6a
Matthew Connealy <mccdqw@mail.missouri.edu>
2021-08-13 13:22:03 -0500

    added social links and changed portfolio label

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,42 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style = {{display: "flex", alignItems: "center", color: "white"}}>
-          <DiCssdeck size="3rem"/> <span>Portfolio</span>
+        <a style = {{display: "flex", alignItems: "center", color: "white", marginBottom: "15px"}}>
+          <DiCssdeck size="3rem"/> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/mccdqw">
+        <AiFillGithub size="3rem"></AiFillGithub>
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/matthew-connealy-818a171a7/">
+        <AiFillLinkedin size="3rem"></AiFillLinkedin>
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
