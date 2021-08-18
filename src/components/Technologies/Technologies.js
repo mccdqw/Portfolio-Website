882efe19088351e6db0bf7d6a774874a10f1fb85
DESKTOP-4M9JO2O\Matthew Connealy <matt.connealy38@gmail.com>
2021-08-17 22:36:07 -0500

    added more list items

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { DiBitbucket, DiCss3Full, DiFirebase, DiHtml5, DiHtml5Multimedia, DiJava, DiMsqlServer, DiMysql, DiPython, DiReact, DiZend } from 'react-icons/di';
+import { DiBitbucket, DiCss3Full, DiDocker, DiFirebase, DiHtml5, DiHtml5Multimedia, DiJava, DiMongodb, DiMsqlServer, DiMysql, DiPython, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <Section id="tech">
+    <br />
     <SectionDivider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range of technologies 
+      I've worked with a range of technologies, and 
+      am eager to learn more as I grow as a software developer.
     </SectionText>
     <List>
       <ListItem>
@@ -27,15 +29,14 @@ const Technologies = () =>  (
         </ListContainer>
       </ListItem>
       <ListItem>
-        <div>
-          <DiMsqlServer size="3rem"></DiMsqlServer>  
+        <div>  
           <DiMysql size="3rem"></DiMysql>
         </div>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Microsoft SQL Server<br />
-            MySQL <br />
+            SQL/MySQL <br />
             ASP.Net         
           </ListParagraph>
         </ListContainer>
@@ -49,8 +50,44 @@ const Technologies = () =>  (
           <ListTitle>Languages</ListTitle>
           <ListParagraph>
             Java<br />
-            C#<br />
-            Python         
+            Python<br />
+            C#         
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <div>
+          <DiDocker size="3rem"></DiDocker> 
+          <DiMongodb size="3rem"></DiMongodb>
+        </div>
+        <ListContainer>
+          <ListTitle>Technology to Learn</ListTitle>
+          <ListParagraph>
+            Docker/Kubernetes<br />
+            MongoDB<br />
+            C++         
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Skills to Learn</ListTitle>
+          <ListParagraph>
+            AI/Machine Learning<br />
+            Blockchain<br />
+            Algorithmic Trading         
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <div>  
+          <DiMsqlServer size="3rem"></DiMsqlServer>
+        </div>
+        <ListContainer>
+          <ListTitle>Currently Learning</ListTitle>
+          <ListParagraph>
+            Microsoft SQL Server<br />
+            ASP.Net         
           </ListParagraph>
         </ListContainer>
       </ListItem>
