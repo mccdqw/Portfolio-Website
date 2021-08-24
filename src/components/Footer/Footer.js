92ab90263183ecafdb28e84311a011c7953f7417
DESKTOP-4M9JO2O\Matthew Connealy <matt.connealy38@gmail.com>
2021-08-23 22:54:40 -0500

    added footer text and links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,29 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:123-456-7890">713-398-9502</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:matt.connealy38@gmail.com">matt.connealy38@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Future business launch, stay tuned...</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/mccdqw">
+          <AiFillGithub size="3rem"></AiFillGithub>
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/matthew-connealy-818a171a7/">
+          <AiFillLinkedin size="3rem"></AiFillLinkedin>
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
