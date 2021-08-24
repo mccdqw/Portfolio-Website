6a4c0e5ae918ef9a00563ce6631fd82104e958f5
DESKTOP-4M9JO2O\Matthew Connealy <matt.connealy38@gmail.com>
2021-08-23 22:30:35 -0500

    added info and changed name

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AccomplishmentsStyles';
+import { data } from '../../constants/constants'
+
+
+const Accomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Accomplishments</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Accomplishments;
